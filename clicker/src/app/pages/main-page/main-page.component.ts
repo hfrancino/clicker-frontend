7ae41fabfc6c04e8673e5517ae9ab42d0f66d0f8
hfrancino <nuriafrancinoxd@gmail.com>
2024-09-03 19:23:06 +0200

    .feat Crear componentes para la main page

--- clicker/src/app/pages/main-page/main-page.component.ts
+++ clicker/src/app/pages/main-page/main-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { ClickerZoneComponent } from '../clicker-zone/clicker-zone.component';
+import { MenuScreenComponent } from '../menu-screen/menu-screen.component';
+import { OptionsMenuComponent } from '../options-menu/options-menu.component';
+
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [ClickerZoneComponent],
+  imports: [ClickerZoneComponent, MenuScreenComponent, OptionsMenuComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
---
+++ clicker/src/app/pages/menu-screen/menu-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-screen.component.html',
+  styleUrl: './menu-screen.component.css'
+})
+export class MenuScreenComponent {
+
+}
